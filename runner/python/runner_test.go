c3c1169bf27fa1d89882b3e6256a6174c76914d7
曹亚文 <caoyawen@meican.com>
2018-05-10 03:04:27 +0800

    解决了读取的问题

--- runner/python/runner_test.go
+++ runner/python/runner_test.go
@@ -2,17 +2,17 @@ package python
 
 import (
 	"context"
-	"os"
 	"testing"
 
 	"fmt"
 	"io"
 	"time"
 
-	"io/ioutil"
 	"strings"
 
 	"os/exec"
+
+	"bufio"
 
 	"github.com/stretchr/testify/assert"
 )
@@ -30,9 +30,35 @@ func TestRunner(t *testing.T) {
 		//writer.Write([]byte("EOF"))
 		writer.Close()
 	}()
-	wait, err := Run(ctx, script, reader, os.Stdout)
+	outReader, outWriter := io.Pipe()
+
+	theOutReader := bufio.NewReader(outReader)
+	readDone := make(chan int)
+	go func() {
+		var err error = nil
+		var data string
+		for ; ; data, err = theOutReader.ReadString('\n') {
+			if err != nil {
+				break
+			}
+			fmt.Printf("get: %s", data)
+		}
+		fmt.Println("err is ", err)
+		close(readDone)
+	}()
+	wait, err := Run(ctx, script, reader, outWriter)
 	<-wait
+	fmt.Println("wait complete")
+	<-readDone
 	assert.Equal(t, err, nil, "应该为nil")
+}
+
+type Output struct {
+}
+
+func (inst *Output) Write(p []byte) (int, error) {
+	fmt.Printf("->%s<-\n", string(p))
+	return len(p), nil
 }
 
 func TestFileRunner(t *testing.T) {
@@ -40,8 +66,32 @@ func TestFileRunner(t *testing.T) {
 	script := "/Users/caoyawen/GOPATH/src/github.com/bobcyw/octopus/runner/python/testPython/hello.py"
 	cmd := exec.CommandContext(ctx, "python3", script)
 	writer, err := cmd.StdinPipe()
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
+	reader, err := cmd.StdoutPipe()
+
+	//outputReader := bufio.NewReader(reader)
+	fmt.Println("start")
+	go func() {
+		//var err error = nil
+		//var data byte
+		buff := make([]byte, 1024)
+		for {
+			_, err := reader.Read(buff)
+			if err != nil {
+				fmt.Println("get error ", err)
+				break
+			}
+		}
+
+		//for ; err == nil; data, err = outputReader.ReadByte() {
+		//	fmt.Println("get: ", data, err)
+		//}
+		//fmt.Println("err is ", err)
+	}()
+	fmt.Println("Step1")
+
+	//out := Output{}
+	//cmd.Stdout = &out
+	//cmd.Stderr = &out
 	if err != nil {
 		t.Fatal(err.Error())
 	}
@@ -55,9 +105,11 @@ func TestFileRunner(t *testing.T) {
 		}
 		close(done)
 	}()
+	fmt.Println("Step2")
 
 	go func() {
 		for i := 0; i < 4; i++ {
+			fmt.Println("write ", i)
 			io.WriteString(writer, fmt.Sprintf("[%d]\n", i))
 			time.Sleep(1 * time.Second)
 			//writer.Write([]byte(fmt.Sprintf("[%d]", i)))
@@ -66,7 +118,7 @@ func TestFileRunner(t *testing.T) {
 		//writer.Write([]byte("EOF"))
 		writer.Close()
 	}()
-
+	fmt.Println("waiting...")
 	<-done
 }
 
@@ -75,17 +127,25 @@ func TestPipeline(t *testing.T) {
 	done := make(chan int)
 	go func() {
 		for i := 0; i < 10; i++ {
-			io.Copy(writer, strings.NewReader(fmt.Sprintf("Hello %d", i)))
-			writer.Close()
+			io.Copy(writer, strings.NewReader(fmt.Sprintf("Hello %d\n", i)))
 			time.Sleep(time.Second)
 		}
+		reader.Close()
 		close(done)
 	}()
 	go func() {
+		theReader := bufio.NewReader(reader)
 		for {
-			data, _ := ioutil.ReadAll(reader)
-			fmt.Println("receive:", string(data))
+			if text, err := theReader.ReadString('\n'); err != nil {
+				fmt.Println("got error ", err)
+				break
+			} else {
+				fmt.Println(text)
+			}
+			//data, _ := ioutil.ReadAll(reader)
+			//fmt.Println("receive:", string(data))
 		}
+
 	}()
 	<-done
 	fmt.Println("all complete")
